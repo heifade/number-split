b825ac2ec3601375b0d2ae50bdca54d57c5ea703
heifade <heifade@126.com>
2020-04-24 17:07:27 +0800

    去掉 bin

---
+++ typings/index.d.ts
@@ -0,0 +1,2 @@
+import { numberSplit } from "./numberSplit";
+export { numberSplit };
---
+++ typings/numberSplit.d.ts
@@ -0,0 +1,6 @@
+/**
+ * 数字切分。如：千分位
+ * @param value 值
+ * @param length 切分长度
+ */
+export declare function numberSplit(value: string | number, length?: number): string;
